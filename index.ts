14b6a65a286dffd23bc194b10e0d9c98e331e9af
Arsenkzn <archi1504@gmail.com>
2025-02-26 14:42:40 +0300

    сделано

---
+++ index.ts
@@ -0,0 +1,31 @@
+//Напишите и типизируйте функцию, выполняющую запрос за данными по переданному URL. Выведите их в консоль в формате: "ID: id, Email: email".
+
+interface Comment {
+  id: number;
+  email: string;
+}
+
+const COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments';
+
+const getData = (url: string): Promise<Comment[]> => {
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => data as Comment[]);
+  // Your code here...
+}
+
+getData(COMMENTS_URL)
+  .then(data => {
+    data.forEach(item => {
+      console.log(`ID: ${item.id}, Email: ${item.email}`);
+    });
+    // Your code here...
+  });
+
+/**
+ * ID: 1, Email: Eliseo...
+ * ID: 2, Email: Jayne_Kuhic...
+ * ID: 3, Email: Nikita...
+ * ID: 4, Email: Lew...
+ * ...
+ */
