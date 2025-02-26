0f813025aff3000d8c565f72d4c8ff2be65a08c7
Arsenkzn <archi1504@gmail.com>
2025-02-26 14:40:57 +0300

    начало

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+//Напишите и типизируйте функцию, выполняющую запрос за данными по переданному URL. Выведите их в консоль в формате: "ID: id, Email: email".
+const COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments';
+const getData = (url) => {
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => data);
+    // Your code here...
+};
+getData(COMMENTS_URL)
+    .then(data => {
+    data.forEach(item => {
+        console.log(`ID: ${item.id}, Email: ${item.email}`);
+    });
+    // Your code here...
+});
+/**
+ * ID: 1, Email: Eliseo...
+ * ID: 2, Email: Jayne_Kuhic...
+ * ID: 3, Email: Nikita...
+ * ID: 4, Email: Lew...
+ * ...
+ */ 
